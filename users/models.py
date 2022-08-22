03adca8b3d72b008977cbb58fe1e650461349b8e
ravindra <ravindra.tagline@gmail.com>
2022-08-22 19:12:08 +0530

    Create managers.py and custom users model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from .managers import UserManager
+
+
+class User(AbstractBaseUser):
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(
+        _("username"),
+        max_length=150,
+        unique=True,
+        help_text=_(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[username_validator],
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+        blank=True,
+        null=True,
+    )
+    email = models.EmailField(max_length=255, unique=True, db_index=True)
+    name = models.CharField(max_length=255, verbose_name="Name")
+
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_(
+            "Designates whether the user can log into this admin site."),
+    )
+
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.name
---
+++ users/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import User
+
+
+"""TODO User serializer"""
+
+class UserSerializer(ModelSerializer):
+    pass
